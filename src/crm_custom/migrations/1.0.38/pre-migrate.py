942efb1ea18bc6df5d1eba638928bdbbc9f766e7
algnot <thanawat.tongla@gmail.com>
2026-06-28 21:08:06 +0700

    fix up

--- src/crm_custom/migrations/1.0.38/pre-migrate.py
+++ src/crm_custom/migrations/1.0.38/pre-migrate.py
@@ -1,6 +1,17 @@
 def migrate(cr, version):
     cr.execute(
         """
+        SELECT EXISTS (
+            SELECT FROM information_schema.tables
+            WHERE table_name = 'partner_warranty_product'
+        )
+        """
+    )
+    if not cr.fetchone()[0]:
+        return
+
+    cr.execute(
+        """
         SELECT column_name
         FROM information_schema.columns
         WHERE table_name = 'partner_warranty_product' AND column_name = 'image'
